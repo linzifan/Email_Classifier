4d53618cd38513466d6a69e42a0fd986f89c0ded
linzifan <zl2364@columbia.edu>
2015-03-17 17:49:25 -0400

    task1

--- first.R
+++ first.R
@@ -1,25 +1,54 @@
+# RESULTS - Used to check
 load("/Users/linzifan/Documents/R practice/Email Classifier/TrainingMessages.rda")
 # example without attachment: 2096.8aecfec50aa2ec00803e8200e0d91399
-trainMessages[[1]]$header
+trainMessages[[1]]
 # example with attachment: 01336.82adb611b4bea7ae97c57911d3152cee
 trainMessages[[15]]
 length(trainMessages[[15]]$header)
+class(trainMessages[[15]]$attachment$`1`)
 
 
-setwd("~/Documents/R practice/Email Classifier")
+# Explore step by step
+setwd("~/Documents/R practice/Email Classifier/SpamAssassinTraining/easy_ham")
+# example 2096.8aecfec50aa2ec00803e8200e0d91399.txt
+filename <- "2096.8aecfec50aa2ec00803e8200e0d91399"
+txt <- readLines(filename, warn = FALSE)
 
-list.files()[2] # extract name from a folder
-file.info(list.files()[2])
-test <- readLines(list.files()[2]) # read each line, form a character vector
-# body is all the text after the first blank line following header and up to any attachment
-ind <- which.max(test=="") # index number of the first blank line
-# check without attachment
-body <- test[-(1:ind)]
+splitHeader <- function(txt){
+  # body is all the text after the first blank line following header and up to any attachment
+  isblank <- txt == ""
+  ind <- which(isblank)[1] # index number of the first blank line
+  # check whether there is a "From Line" at the beginning
+  if(grepl("^From ", txt[1])) {
+    fromLine <- txt[1]
+    start <- 2
+  } else {
+    fromLine <- ""
+    start <- 1
+  }
+  list(header = txt[start:(ind - 1)],
+       body = txt[-(1:ind)],
+       fromLine = fromLine)
+}
+
+# splitHeader(txt)
 
 
 
+makeHeader <- function(txt, asVector = TRUE){
+  con <- textConnection(txt)
+  on.exit(close(con))    
+  h <- read.dcf(con, all = TRUE)  # a data frame with one row
+  # turn it into a character vector
+  if (asVector){
+    structure(unlist(h),
+              names = rep(names(h), sapply(h, function(x)
+                if(is.list(x))
+                  length(x[[1]])
+                else
+                  1L)))
+  } else {h}
+}
 
-strsplit(test[2:(ind-1)],":")
-write.dcf(test[2:(ind-1)])
-class(read.dcf(list.files()[2]))
-class(trainMessages[[1]]$header)
+# makeHeader(splitHeader(txt)$header)
+
